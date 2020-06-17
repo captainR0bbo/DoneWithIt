1a4853964aaa794c674ba5731e729903c8192351
captainR0bbo <56075984+captainR0bbo@users.noreply.github.com>
2020-06-16 22:11:27 -0400

    Refactor AppText

--- app/components/AppText.js
+++ app/components/AppText.js
@@ -1,4 +1,9 @@
-import { StyleSheet, Platform } from "react-native";
+import React from "react";
+import { Text, Platform, StyleSheet } from "react-native";
+
+function AppText(props) {
+  return <Text style={styles.text}>{props.children}</Text>;
+}
 
 const styles = StyleSheet.create({
   text: {
@@ -16,4 +21,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default styles;
+export default AppText;
