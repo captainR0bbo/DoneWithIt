62c33ef0faf96190ca4abf28e01cfa506fbd4d49
captainR0bbo <56075984+captainR0bbo@users.noreply.github.com>
2020-06-17 20:57:55 -0400

    Finish icons on view image screen

--- App.js
+++ App.js
@@ -3,9 +3,10 @@ import { StyleSheet, Platform, StatusBar, View } from "react-native";
 
 import Card from "./app/components/Card";
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
-  return <ListingDetailsScreen />;
+  return <ViewImageScreen />;
 }
 
 const styles = StyleSheet.create({
