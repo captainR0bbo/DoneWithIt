e188a7ccea41a634d6eee5b5ca7ce2fe7bb71792
captainR0bbo <56075984+captainR0bbo@users.noreply.github.com>
2020-06-17 21:42:58 -0400

    Make list items touchable

--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -27,6 +27,7 @@ function MessagesScreen(props) {
         keyExtractor={(message) => message.id.toString()}
         renderItem={({ item }) => (
           <ListItem
+            onPress={() => console.log("Message selected", item)}
             title={item.title}
             subTitle={item.description}
             image={item.image}
