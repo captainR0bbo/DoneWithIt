6a1bdbee979daf2151579d200fdea96f1ae4ea7b
captainR0bbo <56075984+captainR0bbo@users.noreply.github.com>
2020-06-18 21:37:53 -0400

    Account screen done

--- App.js
+++ App.js
@@ -7,13 +7,11 @@ import ViewImageScreen from "./app/screens/ViewImageScreen";
 import MessagesScreen from "./app/screens/MessagesScreen";
 import Screen from "./app/components/Screen";
 import Icon from "./app/components/Icon";
+import ListItem from "./app/components/ListItem";
+import AccountScreen from "./app/screens/AccountScreen";
 
 export default function App() {
-  return (
-    <Screen>
-      <Icon name="email" size={50} backgroundColor="red" iconColor="white" />
-    </Screen>
-  );
+  return <AccountScreen />;
 }
 
 const styles = StyleSheet.create({
