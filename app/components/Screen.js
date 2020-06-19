4bce81e3c1267559318b4bb7d2da1880ee562093
captainR0bbo <56075984+captainR0bbo@users.noreply.github.com>
2020-06-18 21:48:34 -0400

    Listing screen created

--- app/components/Screen.js
+++ app/components/Screen.js
@@ -3,7 +3,11 @@ import Constants from "expo-constants";
 import { StyleSheet, SafeAreaView, View } from "react-native";
 
 function Screen({ children, style }) {
-  return <SafeAreaView style={[styles.screen, style]}>{children}</SafeAreaView>;
+  return (
+    <SafeAreaView style={[styles.screen, style]}>
+      <View style={style}>{children}</View>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
