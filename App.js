5462017b90ebac9f24902f156380db62c790d7fc
captainR0bbo <56075984+captainR0bbo@users.noreply.github.com>
2020-06-18 22:09:42 -0400

    App text input component created

--- App.js
+++ App.js
@@ -1,18 +1,25 @@
-import React from "react";
-import { StyleSheet, Platform, StatusBar, View } from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Platform,
+  StatusBar,
+  View,
+  TextInput,
+  Text,
+} from "react-native";
 
-import Card from "./app/components/Card";
-import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
-import MessagesScreen from "./app/screens/MessagesScreen";
-import Screen from "./app/components/Screen";
-import Icon from "./app/components/Icon";
-import ListItem from "./app/components/ListItem";
-import AccountScreen from "./app/screens/AccountScreen";
 import ListingsScreen from "./app/screens/ListingsScreen";
+import Screen from "./app/components/Screen";
+import AppTextInput from "./app/components/AppTextInput";
 
 export default function App() {
-  return <ListingsScreen />;
+  const [firstName, setFirstName] = useState("");
+
+  return (
+    <Screen>
+      <AppTextInput placeholder="Username" icon="email" />
+    </Screen>
+  );
 }
 
 const styles = StyleSheet.create({
