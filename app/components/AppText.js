f4ff63259ac4d7a31fb3716b714b36a8101b2f77
captainR0bbo <56075984+captainR0bbo@users.noreply.github.com>
2020-06-19 21:29:59 -0400

    Extract styles to a style config file

--- app/components/AppText.js
+++ app/components/AppText.js
@@ -1,24 +1,10 @@
 import React from "react";
-import { Text, Platform, StyleSheet } from "react-native";
+import { Text } from "react-native";
+
+import defaultStyles from "../config/styles";
 
 function AppText({ children, style }) {
-  return <Text style={[styles.text, style]}>{children}</Text>;
+  return <Text style={[defaultStyles.text, style]}>{children}</Text>;
 }
-
-const styles = StyleSheet.create({
-  text: {
-    color: "black",
-    ...Platform.select({
-      ios: {
-        fontSize: 20,
-        fontFamily: "Avenir",
-      },
-      android: {
-        fontSize: 18,
-        fontFamily: "Roboto",
-      },
-    }),
-  },
-});
 
 export default AppText;
