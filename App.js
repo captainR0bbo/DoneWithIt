bbaaf359689576b08263a0aabc450c8bc1e40fd6
captainR0bbo <56075984+captainR0bbo@users.noreply.github.com>
2020-06-21 20:50:26 -0400

    Created App picker component

--- App.js
+++ App.js
@@ -6,18 +6,35 @@ import {
   View,
   TextInput,
   Text,
+  Switch,
 } from "react-native";
 
 import ListingsScreen from "./app/screens/ListingsScreen";
 import Screen from "./app/components/Screen";
 import AppTextInput from "./app/components/AppTextInput";
+import AppPicker from "./app/components/AppPicker";
+import AppText from "./app/components/AppText";
+
+const categories = [
+  { label: "Furniture", value: 1 },
+  { label: "Clothing", value: 2 },
+  { label: "Cameras", value: 3 },
+];
 
 export default function App() {
-  const [firstName, setFirstName] = useState("");
+  const [isNew, setIsNew] = useState(false);
+  const [category, setCategory] = useState(categories[0]);
 
   return (
     <Screen>
-      <AppTextInput placeholder="Username" icon="email" />
+      <AppPicker
+        selectedItem={category}
+        onSelectItem={(item) => setCategory(item)}
+        items={categories}
+        icon="apps"
+        placeholder="Category"
+      />
+      <AppTextInput icon="email" placeholder="Email" />
     </Screen>
   );
 }
