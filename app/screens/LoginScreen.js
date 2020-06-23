cfc62937dc845a4fcfc6aa613d201b2819423bab
captainR0bbo <56075984+captainR0bbo@users.noreply.github.com>
2020-06-23 16:53:17 -0400

    Submit button component

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -6,6 +6,7 @@ import * as Yup from "yup";
 import AppButton from "../components/AppButton";
 import AppFormField from "../components/AppFormField";
 import Screen from "../components/Screen";
+import SubmitButton from "../components/SubmitButton";
 
 const validationSchema = Yup.object().shape({
   email: Yup.string().required().email().label("Email"),
@@ -21,7 +22,7 @@ function LoginScreen(props) {
         onSubmit={(values) => console.log(values)}
         validationSchema={validationSchema}
       >
-        {({ handleChange, handleSubmit, errors, setFieldTouched, touched }) => (
+        {() => (
           <>
             <AppFormField
               autoCapitalize="none"
@@ -41,7 +42,7 @@ function LoginScreen(props) {
               secureTextEntry
               textContentType="password"
             />
-            <AppButton title="Login" onPress={handleSubmit} />
+            <SubmitButton title="Login" />
           </>
         )}
       </Formik>
