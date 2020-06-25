dc4bae8cec7969f55f8c16e9cd8d22e6f907bcad
captainR0bbo <56075984+captainR0bbo@users.noreply.github.com>
2020-06-24 21:28:16 -0400

    Register screen

--- App.js
+++ App.js
@@ -1,35 +1,6 @@
 import React, { useState } from "react";
-import {
-  StyleSheet,
-  Platform,
-  StatusBar,
-  View,
-  TextInput,
-  Text,
-  Switch,
-} from "react-native";
-
-import ListingsScreen from "./app/screens/ListingsScreen";
-import Screen from "./app/components/Screen";
-import AppTextInput from "./app/components/AppTextInput";
-import AppPicker from "./app/components/AppPicker";
-import AppText from "./app/components/AppText";
-import LoginScreen from "./app/screens/LoginScreen";
-
-const categories = [
-  { label: "Furniture", value: 1 },
-  { label: "Clothing", value: 2 },
-  { label: "Cameras", value: 3 },
-];
+import RegisterScreen from "./app/screens/RegisterScreen";
 
 export default function App() {
-  return <LoginScreen />;
+  return <RegisterScreen />;
 }
-
-const styles = StyleSheet.create({
-  cards: {
-    backgroundColor: "#f8f4f4",
-    padding: 20,
-    paddingTop: 100,
-  },
-});
