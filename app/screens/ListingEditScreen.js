7d13d6bc7a7f0d3a318939517aa5d6f8e3f00746
captainR0bbo <56075984+captainR0bbo@users.noreply.github.com>
2020-06-30 20:55:26 -0400

    Edit listing detail screen

---
+++ app/screens/ListingEditScreen.js
@@ -0,0 +1,73 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+import * as Yup from "yup";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+import {
+  AppForm,
+  AppFormField,
+  AppFormPicker,
+  SubmitButton,
+} from "../components/forms";
+import Screen from "../components/Screen";
+
+const validationSchema = Yup.object().shape({
+  title: Yup.string().required().label("Title"),
+  price: Yup.number().required().min(1).max(10000).label("Price"),
+  description: Yup.string().optional().label("Description"),
+  category: Yup.object().required().nullable().label("Category"),
+});
+
+const categories = [
+  { value: 1, label: "Furniture" },
+  { value: 2, label: "Electronics" },
+  { value: 3, label: "Outdoor" },
+];
+
+function ListingEditScreen(props) {
+  return (
+    <Screen style={styles.container}>
+      <AppForm
+        initialValues={{ title: "", price: 0, description: "", category: null }}
+        onSubmit={(values) => console.log(values)}
+        validationSchema={validationSchema}
+      >
+        <AppFormField
+          autoCapitalize="words"
+          autoCorrect={false}
+          name="title"
+          placeholder="Title"
+          textContentType="name"
+        />
+        <AppFormField
+          //icon="account"
+          keyboardType="decimal-pad"
+          name="price"
+          placeholder="Price"
+          textContentType="none"
+        />
+        <AppFormPicker
+          icon="account"
+          items={categories}
+          name="category"
+          placeholder="Categories"
+        />
+        <AppFormField
+          keyboardType="decimal-pad"
+          name="description"
+          placeholder="Description"
+          textContentType="none"
+        />
+        <SubmitButton title="Post" />
+      </AppForm>
+    </Screen>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+  },
+});
+
+export default ListingEditScreen;
