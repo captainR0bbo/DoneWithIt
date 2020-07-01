ce324a3c599e2232a550d0696eb98e22e64c94a3
captainR0bbo <56075984+captainR0bbo@users.noreply.github.com>
2020-06-30 21:04:43 -0400

    Move list components to lists folder

--- App.js
+++ App.js
@@ -1,7 +1,14 @@
 import React, { useState } from "react";
+import AccountScreen from "./app/screens/AccountScreen";
 import RegisterScreen from "./app/screens/RegisterScreen";
 import LoginScreen from "./app/screens/LoginScreen";
+import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
+import ListingEditScreen from "./app/screens/ListingEditScreen";
+import ListingsScreen from "./app/screens/ListingsScreen";
+import MessagesScreen from "./app/screens/MessagesScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  return <LoginScreen />;
+  return <WelcomeScreen />;
 }
--- app/components/lists/ListItemDeleteAction.js
+++ app/components/lists/ListItemDeleteAction.js
@@ -2,7 +2,7 @@ import React from "react";
 import { View, StyleSheet } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-import colors from "../config/colors";
+import colors from "../../config/colors";
 import { Colors } from "react-native/Libraries/NewAppScreen";
 import { TouchableHighlight } from "react-native-gesture-handler";
 
