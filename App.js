3b13302b8746c87727be6f9309f396ff469132f3
captainR0bbo <56075984+captainR0bbo@users.noreply.github.com>
2020-07-06 19:58:47 -0400

    install expo image picker

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AccountScreen from "./app/screens/AccountScreen";
 import RegisterScreen from "./app/screens/RegisterScreen";
 import LoginScreen from "./app/screens/LoginScreen";
@@ -8,7 +8,13 @@ import ListingsScreen from "./app/screens/ListingsScreen";
 import MessagesScreen from "./app/screens/MessagesScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
+import Screen from "./app/components/Screen";
+import * as ImagePicker from "expo-image-picker";
 
 export default function App() {
-  return <ListingEditScreen />;
+  useEffect(async () => {
+    const result = await ImagePicker.requestCameraRollPermissionsAsync();
+  }, []);
+
+  return <Screen></Screen>;
 }
