30465f8bd9cee3e147c5ab3c4fe6b440bc1e7429
captainR0bbo <56075984+captainR0bbo@users.noreply.github.com>
2020-07-08 21:11:08 -0400

    Fix category picker item - touchable

--- app/components/CategoryPickerItem.js
+++ app/components/CategoryPickerItem.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, TouchableOpacity } from "react-native";
 
 import Icon from "./Icon";
 import AppText from "./AppText";
@@ -8,8 +8,14 @@ import AppText from "./AppText";
 function CategoryPickerItem({ item, onPress }) {
   return (
     <View style={styles.container}>
-      <Icon backgroundColor={item.backgroundColor} name={item.icon} size={80} />
-      <AppText style={styles.label}>{item.label}</AppText>
+      <TouchableOpacity onPress={onPress}>
+        <Icon
+          backgroundColor={item.backgroundColor}
+          name={item.icon}
+          size={80}
+        />
+        <AppText style={styles.label}>{item.label}</AppText>
+      </TouchableOpacity>
     </View>
   );
 }
